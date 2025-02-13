85ab55cc0fb7ba45083a17bd73a7e7305772e10e
Leandro <le.souza.rosa@gmail.com>
2025-02-13 21:33:52 +0100

    convert to gp classifier

--- distribution/estimators/gp.py
+++ distribution/estimators/gp.py
@@ -4,11 +4,11 @@ import torch
 # torch stuff# GP stuff
 import gpytorch
 from gpytorch.models import ExactGP
-from gpytorch.likelihoods import MultitaskGaussianLikelihood
+from gpytorch.likelihoods import DirichletClassificationLikelihood
 from gpytorch.mlls import ExactMarginalLogLikelihood
 from gpytorch.means import ConstantMean, MultitaskMean
-from gpytorch.kernels import MultitaskKernel, MaternKernel, PeriodicKernel, RBFKernel, PolynomialKernel
-from gpytorch.distributions import MultitaskMultivariateNormal
+from gpytorch.kernels import ScaleKernel, MaternKernel, PeriodicKernel, RBFKernel, PolynomialKernel
+from gpytorch.distributions import MultivariateNormal
 
 def parser_fn(x, cv_size):
     _d = x['data'][:,:,:cv_size]
@@ -23,29 +23,32 @@ class GPModel(torch.nn.Module):
         super(GPModel, self).__init__()
         self.x = kwargs['x']
         self.y = kwargs['y']
-        self.kernel_kwargs = kwargs['kernel_kwargs']
         self.lr = kwargs['lr']
+        n_classes = kwargs['n_classes'] 
+        
+        # hyperparams
+        self.kernel_kwargs = kwargs['kernel_kwargs']
+        self.lh_kwargs = kwargs['likelihood_kwargs'] if 'likelihood_kwargs' in kwargs else {}
+
         self.device = kwargs['device'] if 'device' in kwargs else 'cpu'
 
-        n_tasks = self.y.shape[1]
-        ard_num_dim = self.x.shape[1]
+        self.x = self.x.to(self.device)
+        self.y = self.y.to(self.device)
 
+        print(self.y)
         # define likelihood (lh)
-        self.lh = MultitaskGaussianLikelihood(n_tasks)
+        self.lh = DirichletClassificationLikelihood(self.y, **self.lh_kwargs, learn_additional_noise = True)
+        self.lh = self.lh.to(self.device)
+
+        # The gp model
         self.gp = GPWrap(
                 x = self.x,
                 y = self.y,
-                lk = self.lh,
-                n_tasks = n_tasks,
-                ard_num_dim = ard_num_dim,
+                lh = self.lh,
+                n_classes = n_classes,
                 kernel_kwargs = self.kernel_kwargs
                 )
-
-        # send to device
-        self.lh = self.lh.to(self.device)
         self.gp = self.gp.to(self.device)
-        self.x = self.x.to(self.device)
-        self.y = self.y.to(self.device)
 
         # Find optimal model hyperparameters
         self.gp.train()
@@ -53,9 +56,9 @@ class GPModel(torch.nn.Module):
 
         # ---- Training related definitions
         self.optimizer = torch.optim.Adam(self.gp.parameters(), lr=self.lr)
-        # Includes GaussianLikelihood parameters
-        self.mll = ExactMarginalLogLikelihood(self.lh, self.gp) 
 
+        # Loss 
+        self.mll = ExactMarginalLogLikelihood(self.lh, self.gp) 
         return
         
     def forward(self, x):
@@ -64,7 +67,7 @@ class GPModel(torch.nn.Module):
     def train_iteration(self):
         self.optimizer.zero_grad()
         output = self.gp(self.x)
-        loss = -self.mll(output, self.y)
+        loss = -self.mll(output, self.lh.transformed_targets).sum()
         loss.backward()
         self.optimizer.step()
         return loss
@@ -92,27 +95,27 @@ class GPWrap(ExactGP):
     def __init__(self, **kwargs):
         x = kwargs['x']
         y = kwargs['y']
-        lk = kwargs['lk']
-        ard_num_dim = kwargs['ard_num_dim']
-        n_tasks = kwargs['n_tasks']
+        lh = kwargs['lh']
+        n_classes = kwargs['n_classes']
         kernel_kwargs = kwargs['kernel_kwargs']
         
-        super(GPWrap, self).__init__(x, y, lk)
+        super(GPWrap, self).__init__(x, y, lh)
+        
+        batch_shape = torch.Size((n_classes, ))
 
         # define means module
-        self.mean_module = MultitaskMean(ConstantMean(), num_tasks = n_tasks)
+        self.mean_module = ConstantMean(batch_shape=batch_shape)
         
         # define covariance module
         kernel = \
-                MaternKernel(nu = kernel_kwargs['nu'], ard_num_dims = ard_num_dim) + \
-                PeriodicKernel(ard_num_dims = ard_num_dim) + \
-                RBFKernel(ard_num_dims = ard_num_dim) + \
-                PolynomialKernel(power = kernel_kwargs['power'], ard_num_dims = ard_num_dim)
+                MaternKernel(nu=kernel_kwargs['nu'], batch_shape=batch_shape) + \
+                PeriodicKernel(batch_shape=batch_shape) + \
+                RBFKernel(batch_shape=batch_shape) + \
+                PolynomialKernel(power=kernel_kwargs['power'], batch_shape=batch_shape)
             
-        self.covar_module = MultitaskKernel(
+        self.covar_module = ScaleKernel(
             kernel,
-            num_tasks = n_tasks,
-            rank = 1,
+            batch_shape = batch_shape 
         )
     
         return
@@ -120,4 +123,4 @@ class GPWrap(ExactGP):
     def forward(self, x):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
-        return MultitaskMultivariateNormal(mean_x, covar_x)
+        return MultivariateNormal(mean_x, covar_x)
--- distribution/gp_test.py
+++ distribution/gp_test.py
@@ -1,6 +1,7 @@
 # python stuff
 from matplotlib import pyplot as plt
 from pathlib import Path
+import matplotlib.colors as mcolors
 
 # torch stuff
 import torch
@@ -19,41 +20,49 @@ def p(t):
         print(f'{k}: ', v)
     return
 
-def foo(d, os):
-    res = torch.zeros(d.shape[0], os)
-    for i in range(os):
-        res[:,i] = torch.cos(d[:,0]*torch.pi)**(i+1) + torch.sin(d[:,1]*torch.pi)**(i+1)
-    return res
+def foo(d, nc):
+    res = torch.zeros(d.shape[0])
+    for i in range(d.shape[1]):
+        res += torch.cos(d[:,i]*torch.pi)
+    res = res.round().int()
+    res -= res.min() # labels must be from positive int
+    return res.clamp(0, nc-1) 
 
 if __name__ == "__main__":
     device = 'cuda'
-    n =  15 
+    n =  30 
+    nc = 4 
     nv = 25 
     ds = 2
-    os = 3
     bs = 5
-    lr = 0.3
+    lr = 0.9
     max_iter = 200
     kernel_kwargs = {'nu': 1.5, 'power':2}
+    lh_kwargs = {'alpha_epsilon': 0.01}
+
     model_file = Path.cwd()/'gp_model_test.pt' 
 
     t = PTD(filename='./banana1', batch_size=[n], mode='w')
     t['x'] = MMT.zeros(shape=(n, ds))
-    t['l'] = MMT.zeros(shape=(n, os))
+    t['l'] = MMT.zeros(shape=(n,), dtype=torch.int)
     t['x'] = torch.rand(n, ds)
-    t['l'] = foo(t['x'], os) 
+    t['l'] = foo(t['x'], nc) 
     
     v = PTD(filename='./banana2', batch_size=[nv], mode='w')
     v['x'] = MMT.zeros(shape=(nv, ds))
-    v['l'] = MMT.zeros(shape=(nv, os))
+    v['l'] = MMT.zeros(shape=(nv,), dtype=torch.int)
     v['x'] = torch.rand(nv, ds)
-    v['l'] = foo(v['x'], os) 
-
-    dl = DataLoader(t, batch_size = bs, collate_fn=lambda x:x)
-    for d in dl:
-        p(d)
-
-    model = GPModel(x=t['x'], y=t['l'], lr=lr, kernel_kwargs=kernel_kwargs, device=device)   
+    v['l'] = foo(v['x'], nc) 
+    
+    model = GPModel(
+            x = t['x'],
+            y = t['l'],
+            n_classes = nc,
+            lr = lr,
+            kernel_kwargs = kernel_kwargs,
+            likelihood_kwargs = lh_kwargs,
+            device=device
+            )   
     
     if not model_file.exists():
         print('-- training')
@@ -62,32 +71,36 @@ if __name__ == "__main__":
             if i%10==0: print('Iter %d/%d - Loss: %.3f'%(i + 1, max_iter, loss.item()))
             
         # save model
-        print('saving: ', model.state_dict())
+        print('saving: ', len(model.state_dict()))
         torch.save(model.state_dict(), model_file)
     else:
         sd = torch.load(model_file)
-        print('loading: ', sd)
+        print('loading: ', len(sd))
         model.load_state_dict(sd)
 
     # Set into eval mode
     model.eval()
-    
-    def scale(x):
-        return (x-t['l'].min())/(t['l'].max()-t['l'].min())
-    
-    fig, axs = plt.subplots(1, os, subplot_kw={'projection': '3d'}, figsize=(os*4, 4))
-    for i in range(os):
-        axs[i].plot(t['x'][:,0], t['x'][:,1], scale(t['l'][:,i]), '.b', label='train')
-        axs[i].plot(v['x'][:,0], v['x'][:,1], scale(v['l'][:,i]), '.r', label='test')
+
+    pallet = list(mcolors.XKCD_COLORS)
+    cs = []
+    for i in range(nc):
+        cs.append(pallet[i])
+
+    fig, axs = plt.subplots(1, nc+1, subplot_kw={'projection': '3d'}, figsize=((nc+1)*4, 4))
+
+    axs[0].scatter(t['x'][:,0], t['x'][:,1], marker='.', c=[cs[x] for x in t['l']], label='train')
+    axs[0].scatter(v['x'][:,0], v['x'][:,1], marker='s', c=[cs[x] for x in v['l']], label='test')
 
     # Make predictions
     with torch.no_grad(), gpytorch.settings.fast_pred_var():
         predictions = model.get_likelihood(v['x'].to(device))
         mean = predictions.mean
+        print('mean: ', mean)
+        print('mean loc: ', predictions.loc)
         lower, upper = predictions.confidence_region()
 
-    for i in range(os):
-        axs[i].plot(v['x'][:,0], v['x'][:,1], scale(mean[:,i].cpu()), '.g', label='pred')
+    axs[0].scatter(v['x'][:,0], v['x'][:,1], marker='v', c=[cs[x] for x in mean], label='pred')
+    axs[0].legend()
 
     # plot confidence
     n_grid = 25 
@@ -104,18 +117,16 @@ if __name__ == "__main__":
         conf = (upper-lower)
 
     conf = conf.cpu()
-    for i in range(os):
-        x = t[:,0]
-        y = t[:,1]
-        z = conf[:,i]
-        axs[i].plot_surface(
-                x.reshape(n_grid, n_grid),
-                y.reshape(n_grid, n_grid),
-                z.reshape(n_grid, n_grid),
+    for i in range(nc):
+        x = t[:,0].reshape(n_grid, n_grid)
+        y = t[:,1].reshape(n_grid, n_grid)
+        z = conf[i].reshape(n_grid, n_grid)
+        axs[i+1].plot_surface(
+                x, y, z,
                 cmap = plt.cm.coolwarm,
                 alpha = 0.5
                 )
-    plt.legend()
+        axs[i+1].set_title(f'class {i}')
     fig.tight_layout()
     plt.savefig(Path.cwd()/'gp_test.png', dpi=300)
     plt.show()
